3050ebcdf4540d190a0950b07844c4b47a3e0e9f
Lance-Walker-MBP <walkerlance65@gmail.com>
2019-01-30 13:20:07 -0700

    iOS init

---
+++ mobile_dist/CoreNotee_iOS/CoreNotee_iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreNotee_iOS
+//
+//  Created by Lance T. Walker on 1/30/19.
+//  Copyright © 2019 CodeLife-Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ mobile_dist/CoreNotee_iOS/CoreNotee_iOS/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoreNotee_iOS
+//
+//  Created by Lance T. Walker on 1/30/19.
+//  Copyright © 2019 CodeLife-Productions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
